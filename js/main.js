ffd01a938c51c1214440f1aacd15dc51a5e54fb0
Ahmed El Sayed <87061714+Ahmed-El-Sayed-korani@users.noreply.github.com>
2023-03-28 12:37:29 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+var copaya="";
+
+for (i=0 ;i<100;i++) {
+    copaya += `
+    <div class="col-md-3 my-2 text-center">
+    <img src="images/projects/project-1.jpg" alt="" class="w-100">
+    <h4>ahmed el sayed</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, et.</p>
+    </div>`
+    
+}
+
+document.getElementById('demo').innerHTML= copaya
